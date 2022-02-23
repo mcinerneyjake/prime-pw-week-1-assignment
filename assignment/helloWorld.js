81bb1603acb23e5c92b8ee933d2f18471afa9135
Jake McInerney <jakemcinerney1@gmail.com>
2022-02-22 22:28:24 -0600

    added javascript h1 functionality

---
+++ assignment/helloWorld.js
@@ -0,0 +1,32 @@
+/* This JavaScript was originally written by the Youtuber, Dev Ed. Here's the link to his tutorial:
+https://www.youtube.com/watch?v=GUEB9FogoP8
+
+It just looks so cool, I couldn't resist!
+*/
+
+const text = document.querySelector(".name");
+const stringText = text.textContent;
+const splitText = stringText.split("");
+text.textContent = "";
+
+for (let i = 0; i < splitText.length; i++) {
+  text.innerHTML += "<span>" + splitText[i] + "</span>";
+}
+
+let char = 0;
+let timer = setInterval(onTick, 50);
+
+function onTick() {
+  const span = text.querySelectorAll("span")[char];
+  span.classList.add("fade-in");
+  char++;
+  if (char === splitText.length) {
+    complete();
+    return;
+  }
+}
+
+function complete() {
+  clearInterval(timer);
+  timer = null;
+}
