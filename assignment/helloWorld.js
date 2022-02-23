cd8c268473865e4afa7a80508e142024ab691704
Jake McInerney <jakemcinerney1@gmail.com>
2022-02-23 10:21:56 -0600

    added comments on the final two functions in JS

--- assignment/helloWorld.js
+++ assignment/helloWorld.js
@@ -1,3 +1,4 @@
+// This is part of the assignment.
 console.log("Hello World");
 
 /* Attribution and credit to the Youtuber, Dev Ed, and their great work.
@@ -17,16 +18,21 @@ text.textContent = ""; // This line prevents the h1 from being put onto the brow
 // The span tags on each index allow the user to manipulate the text styling in style.css.
 // The innerHTML allows the JS to print the array onto the .name class in index.html.
 // splitText[i] represents each individual character in the h1, including the space between "Jake" and "McInerney".
-// The <span> and </span> creates the span tag, and the splitText[i] is the value within each span.
+// The <span> and </span> creates the span tag for each character (that's what the "+=" is for), and the splitText[i] is the value within each span.
 // splitText.length prevents the loop from looping continuously, and gives the function an endpoint when the length of splitText is reached.
 for (let i = 0; i < splitText.length; i++) {
   text.innerHTML += "<span>" + splitText[i] + "</span>";
 }
 
-// These let variables give a starting point
+// These let variables give a starting point (character = 0) and an interval that runs repeatedly.
+// The setInterval function calls another function, onRefresh, every 50 milliseconds.
 let character = 0;
 let timer = setInterval(onRefresh, 50);
 
+/* The onRefresh function selects the span tags in index.html with the const of "span",
+gives each span a class of "fade-in" with span.classList.add, positively increments each
+character with character++, and calls the clearInterval function of complete() when the 
+entire length of the h1 is reached. */
 function onRefresh() {
   const span = text.querySelectorAll("span")[character];
   span.classList.add("fade-in");
@@ -37,6 +43,8 @@ function onRefresh() {
   }
 }
 
+// The complete() function clears the setInterval above when the entire length of the h1 is reached.
+// The let timer from above is nulled within this function to stop the onRefresh function.
 function complete() {
   clearInterval(timer);
   timer = null;
